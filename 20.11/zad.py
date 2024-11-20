d1a118805f8de5f470703287974485949245a2cf
HAJTI <deadend6464@gmail.com>
2024-11-20 10:47:59 +0100

    chore: Refactor file menu commands in the text editor

--- 20.11/zad.py
+++ 20.11/zad.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog,messagebox
+from tkinter import filedialog, messagebox
 import os
 def open_file():
     file_path=filedialog.askopenfilename(
@@ -58,12 +58,17 @@ scrollbar.config(command=text_area.yview)
 menu_bar=tk.Menu(root)
 file_menu=tk.Menu(menu_bar, tearoff=0)
 file_menu.add_command(label="Otwórz", command=open_file)
-file_menu.add_command(label="Zapisz")
-file_menu.add_command(label="Zapisz jako")
+file_menu.add_command(label="Zapisz", command=save_file)
+file_menu.add_command(label="Zapisz jako", command=save_as_file)
 file_menu.add_separator()
 file_menu.add_command(label="Zakończ", command=close_file)
 file_menu.add_separator()
 file_menu.add_command(label="Wyjdz", command=root.quit)
 menu_bar.add_cascade(label="Plik", menu=file_menu)
 root.config(menu=menu_bar)
+def on_closing():
+    if messagebox.askokcancel("Zamknij", "Czy na pewno chcesz zamknąć program?"):
+        root.destroy()
+root.protocol("WM_DELETE_WINDOW", on_closing)
+save_file.file_path=None
 root.mainloop()
