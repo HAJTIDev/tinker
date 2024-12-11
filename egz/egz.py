a08114cd177eec1e737bfe88ef591d68c98a1968
HAJTI <deadend6464@gmail.com>
2024-12-11 10:41:31 +0100

    refactor: Update GUI layout and styling for adding employees and generating passwords

--- egz/egz.py
+++ egz/egz.py
@@ -5,20 +5,31 @@ class mainn:
     def __init__(self, root):
         self.root = root
         self.root.title("Dodaj pracownika")
-        self.root.geometry("500x400")
-        self.root.resizable(False, False)
+        self.root.geometry("643x400")
+        self.root.configure(bg="#B0C4DE")
 
-        left_frame = ttk.Frame(self.root)
-        left_frame.pack(side=tk.LEFT, padx=10, pady=10)
-
-        right_frame = ttk.Frame(self.root)
-        right_frame.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        label1 = ttk.Label(left_frame, text="Left Box")
-        label1.pack()
-
-        label2 = ttk.Label(right_frame, text="Right Box")
-        label2.pack()
+        frame_left=tk.LabelFrame(self.root,text="Dodaj Pracownika", bg="#B0C4DE")
+        frame_left.place(x=10,y=10,width=260,height=200)
+        
+        tk.Label(frame_left,text="imie",bg="#B0C4DE").grid(row=0,column=0,sticky="w",pady=5)
+        tk.Entry(frame_left).grid(row=0,column=1,pady=5)
+        tk.Label(frame_left,text="nazwisko",bg="#B0C4DE").grid(row=1,column=0,sticky="w",pady=5)
+        tk.Entry(frame_left).grid(row=1,column=1,pady=5)
+        tk.Label(frame_left,text="Stanowisko",bg="#B0C4DE").grid(row=2,column=0,sticky="w",pady=5)
+        ttk.Combobox(frame_left,values=["Kierownik","Starszy Programista","Młodszy Programista","Tester"]).grid(row=2,column=1,pady=5)
+        
+        frame_right=tk.LabelFrame(self.root,text="Generowanie hasła", bg="#B0C4DE")
+        frame_right.place(x=350,y=10,width=280,height=200)
+        
+        tk.Label(frame_right,text="Ile znaków",bg="#B0C4DE").grid(row=0,column=0,sticky="w",pady=5)
+        tk.Entry(frame_right).grid(row=0,column=1,pady=5)
+        tk.Checkbutton(frame_right,text="Małe i wielkie litery",bg="#B0C4DE").grid(row=1,column=0,sticky="w",pady=5)
+        tk.Checkbutton(frame_right,text="cyfry",bg="#B0C4DE").grid(row=2,column=0,sticky="w",pady=5)
+        tk.Checkbutton(frame_right,text="znaki specjalne",bg="#B0C4DE").grid(row=3,column=0,sticky="w",pady=5)
+        tk.Button(frame_right,text="Generuj",bg="#4682B4").grid(row=4,column=0,columnspan=2,pady=5)
+        
+        tk.Button(root,text="Zatwierdz",width=20,bg="#4682B4").place(x=233,y=240)
+        
 
 def main():
     root = tk.Tk()
