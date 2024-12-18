9853a6d62b3716759b02f66e49b767554287bcb4
HAJTI <deadend6464@gmail.com>
2024-12-18 10:44:18 +0100

    refactor: Update GUI layout and styling for adding employees and generating passwords

--- egz2/egz2.py
+++ egz2/egz2.py
@@ -2,31 +2,31 @@ import tkinter as tk
 from tkinter import ttk
 
 def oblicz_cene():
-
     if rodzaj.get() == "Pocztówka":
         cena.set("Cena: 1,0 zł")
     elif rodzaj.get() == "List":
         cena.set("Cena: 1,5 zł")
     elif rodzaj.get() == "Paczka":
         cena.set("Cena: 10,0 zł")
+
+def update_price():
+    oblicz_cene()
+
 root = tk.Tk()
 root.title("Nadaj Przesyłkę")
 root.geometry("500x300")
 root.resizable(False, False)
 rodzaj = tk.StringVar(value="List")
 cena = tk.StringVar(value="Cena: 1,5 zł")
-
 frame1 = tk.LabelFrame(root, text="Rodzaj przesyłki", padx=10, pady=10)
 frame1.place(x=20, y=20, width=150, height=130)
 
-tk.Radiobutton(frame1, text="Pocztówka", variable=rodzaj, value="Pocztówka", command=oblicz_cene).pack(anchor="w")
-tk.Radiobutton(frame1, text="List", variable=rodzaj, value="List", command=oblicz_cene).pack(anchor="w")
-tk.Radiobutton(frame1, text="Paczka", variable=rodzaj, value="Paczka", command=oblicz_cene).pack(anchor="w")
-btn_cena = tk.Button(root, text="Sprawdź Cenę", command=oblicz_cene)
+tk.Radiobutton(frame1, text="Pocztówka", variable=rodzaj, value="Pocztówka").pack(anchor="w")
+tk.Radiobutton(frame1, text="List", variable=rodzaj, value="List").pack(anchor="w")
+tk.Radiobutton(frame1, text="Paczka", variable=rodzaj, value="Paczka").pack(anchor="w")
+btn_cena = tk.Button(root, text="Sprawdź Cenę", command=update_price)
 btn_cena.place(x=30, y=160, width=120, height=25)
-canvas = tk.Canvas(root, width=80, height=50)
-canvas.create_rectangle(5, 5, 75, 45, fill="grey", outline="black")
-canvas.place(x=20, y=200)
+
 label_cena = tk.Label(root, textvariable=cena, font=("Arial", 10, "bold"))
 label_cena.place(x=120, y=220)
 frame2 = tk.LabelFrame(root, text="Dane adresowe", padx=10, pady=10)
