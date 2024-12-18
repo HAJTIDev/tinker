edcceaf95d5da4199349dc9096efe6cb8021d49e
HAJTI <deadend6464@gmail.com>
2024-12-18 09:22:08 +0100

    s

--- egz/egz.py
+++ egz/egz.py
@@ -27,34 +27,33 @@ class mainn:
         frame_right.place(x=350,y=10,width=280,height=200)
         
         tk.Label(frame_right,text="Ile znaków",bg="#B0C4DE").grid(row=0,column=0,sticky="w",pady=5)
-        tk.Entry(frame_right).grid(row=0,column=1,pady=5)
+        self.length_entry = tk.Entry(frame_right)
+        self.length_entry.grid(row=0,column=1,pady=5)
         tk.Checkbutton(frame_right,text="Małe i wielkie litery",bg="#B0C4DE",variable=self.include_letters).grid(row=1,column=0,sticky="w",pady=5)
         tk.Checkbutton(frame_right,text="cyfry",bg="#B0C4DE",variable=self.include_digits).grid(row=2,column=0,sticky="w",pady=5)
         tk.Checkbutton(frame_right,text="znaki specjalne",bg="#B0C4DE",variable=self.include_special).grid(row=3,column=0,sticky="w",pady=5)
-        tk.Button(frame_right,text="Generuj",bg="#4682B4").grid(row=4,column=0,columnspan=2,pady=5)
+        tk.Button(frame_right,text="Generuj",bg="#4682B4", command=self.generatepass).grid(row=4,column=0,columnspan=2,pady=5)
         
         tk.Button(root,text="Zatwierdz",width=20,bg="#4682B4").place(x=233,y=240)
         
     def generatepass(self):
-        rangeOfNum = self.length_var.get()
+        rangeOfNum = int(self.length_entry.get())
         if self.include_letters.get() == False and self.include_digits.get() == False and self.include_special.get() == False:
             messagebox.showerror("ERROR","Nie można wygenerować bez niczego")
-        elif self.length_var.get()<1:
+        elif rangeOfNum < 1:
             messagebox.showerror("ERROR","Nie można wygenerować hasła z jedną literą")
         else:
-            self.password_entry.delete(0, tk.END)
             temp = ""
-            allChars =""
+            allChars = ""
             if self.include_letters.get():
-                allChars+= string.ascii_letters
+                allChars += string.ascii_letters
             if self.include_digits.get():
-                allChars+=string.digits
+                allChars += string.digits
             if self.include_special.get():
-                allChars+=string.punctuation
+                allChars += string.punctuation
             for _ in range(rangeOfNum):
-                temp+=random.choice(allChars)
-            self.password_var.set(temp)
-           
+                temp += random.choice(allChars)
+            messagebox.showinfo("Wygenerowane hasło", temp)
         
 def main():
     root = tk.Tk()
